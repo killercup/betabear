c23713aaf1d73f612c007b57d4511c42a0511a67
Pascal Hertleif <killercup@gmail.com>
2016-01-31 09:42:53 +0100

    Add some benches

--- benches/find_words.rs
+++ benches/find_words.rs
@@ -9,9 +9,11 @@ use test::Bencher;
 static DICTIONARY: &'static str = include_str!("/usr/share/dict/words");
 
 #[bench]
-fn build_1(b: &mut Bencher) {
+fn build_binary_search(b: &mut Bencher) {
     let given_letters = "noraanoraanoraa";
     let word = "aaronaaronaaron";
+
+    b.bytes = given_letters.len() as u64;
 
     b.iter(|| {
         let mut given_letters = given_letters.chars().collect::<Vec<char>>();
@@ -30,9 +32,11 @@ fn build_1(b: &mut Bencher) {
 }
 
 #[bench]
-fn build_1_prepare_first(b: &mut Bencher) {
+fn build_binary_search_prepare_first(b: &mut Bencher) {
     let word = "aaronaaronaaron";
     let given_letters = "noraanoraanoraa";
+
+    b.bytes = given_letters.len() as u64;
 
     let mut given_letters = given_letters.chars().collect::<Vec<char>>();
     given_letters.sort(); // For binary_search down the road
@@ -52,9 +56,11 @@ fn build_1_prepare_first(b: &mut Bencher) {
 }
 
 #[bench]
-fn build_1_check_len_first(b: &mut Bencher) {
+fn build_binary_search_check_len_first(b: &mut Bencher) {
     let given_letters = "noraanoraanoraa";
     let word = "aaronaaronaaron";
+
+    b.bytes = given_letters.len() as u64;
 
     b.iter(|| {
         let mut given_letters = given_letters.chars().collect::<Vec<char>>();
@@ -78,9 +84,11 @@ fn build_1_check_len_first(b: &mut Bencher) {
 }
 
 #[bench]
-fn build_no_binary_search(b: &mut Bencher) {
+fn build_trivial_search(b: &mut Bencher) {
     let given_letters = "noraanoraanoraa";
     let word = "aaronaaronaaron";
+
+    b.bytes = given_letters.len() as u64;
 
     b.iter(|| {
         let mut given_letters = given_letters.chars().collect::<Vec<char>>();
@@ -98,9 +106,11 @@ fn build_no_binary_search(b: &mut Bencher) {
 }
 
 #[bench]
-fn build_no_binary_search_prepared(b: &mut Bencher) {
+fn build_trivial_search_prepared(b: &mut Bencher) {
     let given_letters = "noraanoraanoraa";
     let word = "aaronaaronaaron";
+
+    b.bytes = given_letters.len() as u64;
 
     let given_letters = given_letters.chars().collect::<Vec<char>>();
 
@@ -120,9 +130,39 @@ fn build_no_binary_search_prepared(b: &mut Bencher) {
 }
 
 #[bench]
-fn build_no_binary_search_prepared_and_sorted(b: &mut Bencher) {
+fn build_trivial_search_prepared_len_check(b: &mut Bencher) {
     let given_letters = "noraanoraanoraa";
     let word = "aaronaaronaaron";
+
+    b.bytes = given_letters.len() as u64;
+
+    let given_letters = given_letters.chars().collect::<Vec<char>>();
+
+    b.iter(|| {
+        let mut given_letters = given_letters.clone();
+
+        if given_letters.len() < word.chars().count() {
+            return false;
+        }
+
+        for letter in word.chars() {
+            if let Some(index) = given_letters.iter().position(|&x| x == letter) {
+                given_letters.remove(index);
+            } else {
+                return false;
+            }
+        }
+
+        true
+    })
+}
+
+#[bench]
+fn build_trivial_search_prepared_and_sorted(b: &mut Bencher) {
+    let given_letters = "noraanoraanoraa";
+    let word = "aaronaaronaaron";
+
+    b.bytes = given_letters.len() as u64;
 
     let mut given_letters = given_letters.chars().collect::<Vec<char>>();
     given_letters.sort();
@@ -143,8 +183,63 @@ fn build_no_binary_search_prepared_and_sorted(b: &mut Bencher) {
 }
 
 #[bench]
+fn build_trivial_search_with_chars_everywhere(b: &mut Bencher) {
+    let given_letters = "noraanoraanoraa";
+    let word = "aaronaaronaaron";
+
+    b.bytes = given_letters.len() as u64;
+
+    let given_letters = given_letters.chars().collect::<Vec<char>>();
+    let word = word.chars().collect::<Vec<char>>();
+
+    b.iter(|| {
+        let mut given_letters = given_letters.clone();
+
+        for letter in &word {
+            if let Some(index) = given_letters.iter().position(|&x| x == *letter) {
+                given_letters.remove(index);
+            } else {
+                return false;
+            }
+        }
+
+        true
+    })
+}
+
+#[bench]
+fn build_trivial_search_with_sorted_chars_everywhere(b: &mut Bencher) {
+    let given_letters = "noraanoraanoraa";
+    let word = "aaronaaronaaron";
+
+    b.bytes = given_letters.len() as u64;
+
+    let mut given_letters = given_letters.chars().collect::<Vec<char>>();
+    given_letters.sort();
+
+    let mut word = word.chars().collect::<Vec<char>>();
+    word.sort();
+
+    b.iter(|| {
+        let mut given_letters = given_letters.clone();
+
+        for letter in &word {
+            if let Some(index) = given_letters.iter().position(|&x| x == *letter) {
+                given_letters.remove(index);
+            } else {
+                return false;
+            }
+        }
+
+        true
+    })
+}
+
+#[bench]
 #[ignore]
 fn search_1(b: &mut Bencher) {
+    b.bytes = DICTIONARY.len() as u64;
+
     let letters = "noraa";
     let letters = letters.trim().chars().collect::<Vec<char>>();
 
@@ -163,9 +258,38 @@ fn search_1(b: &mut Bencher) {
 #[bench]
 #[ignore]
 fn search_1_with_regex(b: &mut Bencher) {
+    b.bytes = DICTIONARY.len() as u64;
+
     let letters = "noraa";
 
     let regex = regex::Regex::new(&format!("^[{}]+$", letters))
+                    .expect("String of given letters is malformed.");
+
+    let letters = letters.trim().chars().collect::<Vec<char>>();
+
+    b.iter(|| {
+        let mut matches = DICTIONARY.lines()
+                                    .filter(|word| regex.is_match(word))
+                                    .filter(|word| betabear::build_with_letters(&letters, word))
+                                    .map(|word| (word, word.chars().count()))
+                                    .collect::<Vec<(&str, usize)>>();
+
+        matches.sort_by(|a, b| b.1.cmp(&a.1));
+
+        matches
+    })
+}
+
+#[bench]
+#[ignore]
+fn search_1_with_better_regex(b: &mut Bencher) {
+    b.bytes = DICTIONARY.len() as u64;
+
+    let letters = "noraa";
+
+    let regex = regex::Regex::new(&format!("^[{}]{{1,{num}}}$",
+                                           letters,
+                                           num = letters.chars().count()))
                     .expect("String of given letters is malformed.");
 
     let letters = letters.trim().chars().collect::<Vec<char>>();
